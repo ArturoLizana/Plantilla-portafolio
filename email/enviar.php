cf8f7b708b027572547c073d38e3bc5b7a63dfa1
Arturo Lizana <111970123+ArturoLizana@users.noreply.github.com>
2024-05-07 16:46:55 +0200

    Update enviar.php

--- email/enviar.php
+++ email/enviar.php
@@ -17,14 +17,14 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.hostinger.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'arturo@arlidev.fr';                     //SMTP username
-    $mail->Password   = 'Kurushan1984@';                               //SMTP password
+    $mail->Username   = '';                     //SMTP username
+    $mail->Password   = '';                               //SMTP password
     $mail->SMTPSecure = 'TLS';            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->setFrom('arturo@arlidev.fr', 'Arturo');
-    $mail->addAddress('arturo@arlidev.fr', 'Joe User');     //Add a recipient
+    $mail->setFrom('', 'Arturo');
+    $mail->addAddress('', 'Joe User');     //Add a recipient
  //   $mail->addAddress('ellen@example.com');               //Name is optional
   //  $mail->addReplyTo('info@example.com', 'Information');
  //   $mail->addCC('cc@example.com');
